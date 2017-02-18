4fe1872ec1c5ef1914a719430e4d4246add6534c
Vitalij <kontrabasista@wp.pl>
2017-02-18 14:43:33 +0100

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by m.losK on 2017-02-18.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Not to say Hello world!");
+    }
+}
